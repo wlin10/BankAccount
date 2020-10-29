59e9b0660eaba65c01b17808d938f1211587030c
wlin10 <wlin10@stuy.edu>
2020-10-28 22:11:08 -0400

    4b

--- BankAccount.java
+++ BankAccount.java
@@ -34,4 +34,13 @@ public class BankAccount{
     return false;
   }
 
+  public boolean withdraw(double amount) {
+    if (amount > balance || amount < 0) {
+      return false;
+    } else {
+      balance -= amount;
+      return true;
+    }
+  }
+
 }
